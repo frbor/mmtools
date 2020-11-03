2a308a68c2a919cea2b23f13f355e07abbb9aff5
Fredrik Borg <fredrikb@mnemonic.no>
2020-11-03 20:42:07 +0100

    error handling

--- mmtools/status.py
+++ mmtools/status.py
@@ -4,12 +4,12 @@ import argparse
 import re
 import sys
 import time
-from typing import List, Tuple
+from typing import List, Tuple, Callable
 
 import requests
 
 from . import arguments
-from .mattermost import Channels, Mattermost
+from .mattermost import Mattermost
 
 
 def parseargs() -> argparse.Namespace:
@@ -33,34 +33,49 @@ def parseargs() -> argparse.Namespace:
     return arguments.handle_args(parser, "mmstatus")
 
 
-def get_status(args: argparse.Namespace, mm) -> Tuple[List, List]:
-    channels = mm.init_channels()
+def init_mattermost(args: argparse.Namespace, error: Callable) -> Mattermost:
+    try:
+        return Mattermost(args)
+    except requests.exceptions.ReadTimeout:
+        error(args, "Timeout")
+    except requests.exceptions.ConnectionError:
+        error(args, "Connection error")
 
-    # channel.type == D (Direct)
-    private = [f"{channel.display_name}:{channel.msg_unread_count}"
-               for channel in channels.channels
-               if channel.msg_unread_count and channel.type == "D"
-               and not (args.ignore and re.search(args.ignore, channel.name))]
-    other = [f"{channel.name}:{channel.msg_unread_count}"
-             for channel in channels.channels
-             if channel.msg_unread_count and channel.type != "D"
-             and not (args.ignore and re.search(args.ignore, channel.name))]
 
-    return (private, other)
+def get_status(args: argparse.Namespace, mm: Mattermost, error: Callable) -> Tuple[List, List]:
+    try:
+        channels = mm.init_channels()
+
+        # channel.type == D (Direct)
+        private = [f"{channel.display_name}:{channel.msg_unread_count}"
+                   for channel in channels.channels
+                   if channel.msg_unread_count and channel.type == "D"
+                   and not (args.ignore and re.search(args.ignore, channel.name))]
+        other = [f"{channel.name}:{channel.msg_unread_count}"
+                 for channel in channels.channels
+                 if channel.msg_unread_count and channel.type != "D"
+                 and not (args.ignore and re.search(args.ignore, channel.name))]
+
+        return (private, other)
+    except requests.exceptions.ReadTimeout:
+        error(args, "Timeout")
+    except requests.exceptions.ConnectionError:
+        error(args, "Connection error")
+
+
+def i3blocks_fatal(args, message):
+    msg = f"{args.chat_prefix.strip()} {message}"
+    print(f"{msg}\n{msg}\n#FF0000")
+    sys.exit(0)
 
 
 def i3blocks() -> None:
     """ Output channel status in i3blocks format """
 
     args = parseargs()
-    mm = Mattermost(args)
+    mm = init_mattermost(args, error=i3blocks_fatal)
 
-    try:
-        (private, other) = get_status(args, mm)
-    except requests.exceptions.ConnectionError:
-        msg = f"{args.chat_prefix.strip()} Connection error"
-        print(f"{msg}\n{msg}\n#FF0000")
-        sys.exit(0)
+    (private, other) = get_status(args, mm, error=i3blocks_fatal)
 
     out = args.chat_prefix
 
@@ -80,18 +95,19 @@ def i3blocks() -> None:
         print(args.channel_color)
 
 
+def polybar_fatal(args, message):
+    print(f"%{{F{args.channel_color}}}{args.chat_prefix} {message}")
+    sys.exit(0)
+
+
 def polybar() -> None:
     """ Output channel status in i3blocks format """
 
     args = parseargs()
-    mm = Mattermost(args)
+    mm = init_mattermost(args, error=polybar_fatal)
 
     while True:
-        try:
-            (private, other) = get_status(args, mm)
-        except requests.exceptions.ConnectionError:
-            print(f"%{{F#F00}}{args.chat_prefix.strip()} Connection error")
-            sys.exit(0)
+        (private, other) = get_status(args, mm, polybar_fatal)
 
         private = [f"%{{F{args.user_color}}}{channel}" for channel in private]
         other = [f"%{{F{args.channel_color}}}{channel}" for channel in other]
